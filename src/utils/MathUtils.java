13f76c58c36dfcd77521efefd70e5cc8de6a6df3
Guilherme Pereira <guilhermepereira2917@gmail.com>
2022-07-22 00:26:27 -0300

    Implementação dos métodos InteiroAleatório e InteiroAleatórioEntre

---
+++ src/utils/MathUtils.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class MathUtils {
+    public static int InteiroAleatorio(int maximo) {
+        return InteiroAleatorioEntre(0, maximo);
+    }
+    
+    public static int InteiroAleatorioEntre(int minimo, int maximo) {
+        return (int) ((Math.random() * (maximo - minimo)) + maximo);
+    }
+}
